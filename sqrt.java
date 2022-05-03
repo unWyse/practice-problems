3c138d3e41be299a17236b19ea40bdd4923b9863
Kyle D Wyse <43249343+unWyse@users.noreply.github.com>
2022-05-03 15:39:55 -0500

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,21 @@
+/*
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+Constraints:
+    0 <= x <= 2^31 - 1
+*/
+class Solution {
+    public int mySqrt(int x) {
+        if(x>=2147395600) return 46340; //edge case
+        
+        int i = 0;
+        while(i*i <= x){
+            i++;
+        }
+        return i-1;
+    }
+}
