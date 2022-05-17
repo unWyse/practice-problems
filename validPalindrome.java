a4e1575acebc6b4626e826cb3d950837433777ee
Kyle D Wyse <43249343+unWyse@users.noreply.github.com>
2022-05-17 14:53:32 -0500

    Create validPalindrome.java

---
+++ validPalindrome.java
@@ -0,0 +1,18 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into 
+lowercase letters and removing all non-alphanumeric characters, it reads 
+the same forward and backward. Alphanumeric characters include letters 
+and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Constraints:
+    1 <= s.length <= 2 * 10^5
+    s consists only of printable ASCII characters.
+*/
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        return s.equals(new StringBuilder(s).reverse().toString());
+    }
+}
